dfe8d322b4fdd00f15f73abffeecf7f6ebee61d7
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-10-27 21:42:03 -0300

    cards update

--- js/positivus.js
+++ js/positivus.js
@@ -36,10 +36,6 @@ new WOW().init();
 
 
 
-
-
-
-
 function showTeam() {
   const teamMemberOne = document.getElementById('teamBoxOne')
   teamMemberOne.style.display = 'block';
@@ -48,3 +44,37 @@ function showTeam() {
   teamMemberTwo.style.display = 'block';
 }
 
+
+// Function to toggle the image and background color for each card
+function setupToggle(elementId, iconId, originalSrc, newSrc) {
+  const actionStep = document.getElementById(elementId);
+  const iconImage = document.getElementById(iconId);
+  let isIconToggled = false; 
+
+  iconImage.src = originalSrc; // Initialize the image
+
+  actionStep.addEventListener('click', function() {
+      if (isIconToggled) {
+          actionStep.style.backgroundColor = ''; 
+          iconImage.src = originalSrc; 
+          iconImage.alt = 'Original Image'; 
+          iconImage.style.width = '28px'; 
+          iconImage.style.height = '28px'; 
+      } else {
+          actionStep.style.backgroundColor = '#B9FF66'; 
+          iconImage.src = newSrc; 
+          iconImage.alt = 'Toggle Image'; 
+          iconImage.style.width = '28px'; 
+          iconImage.style.height = '28px'; 
+      }
+      isIconToggled = !isIconToggled; 
+  });
+}
+
+// Set up toggles for each card
+setupToggle('stepInfo', 'plusIcon', 'image/Icon(1).png', 'image/Negativeicon.png');
+setupToggle('actionStep', 'toggleIcon', 'image/Icon(1).png', 'image/Negativeicon.png');
+setupToggle('actionStep1', 'toggleIcon1', 'image/Icon(1).png', 'image/Negativeicon.png');
+setupToggle('actionStep2', 'toggleIcon2', 'image/Icon(1).png', 'image/Negativeicon.png');
+setupToggle('actionStep3', 'toggleIcon3', 'image/Icon(1).png', 'image/Negativeicon.png');
+setupToggle('actionStep4', 'toggleIcon4', 'image/Icon(1).png', 'image/Negativeicon.png');
